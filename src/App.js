0ebfa5557ed55bc70670f0d1bb41b7af2b54f63d
ArunReddy99-ux <arunreddyveerelly2003@gmail.com>
2024-12-02 15:54:21 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { useState } from "react";
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
     </div>
   );
 }
-
-export default App;
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  function handleadd() {
+    setStep((step) => step + 1);
+  }
+  function handleneg() {
+    setStep((step) => step - 1);
+  }
+  function handlecountadd() {
+    setCount((c) => Number(step) + c);
+  }
+  function handlecountsub() {
+    setCount((c) => c - Number(step));
+  }
+  const date = new Date("june 21 2027");
+  date.setDate(date.getDate() + count);
+  return (
+    <div>
+      <div>
+        <button onClick={handleneg}>-</button>
+        <span>Step:{step}</span>
+        <button onClick={handleadd}>+</button>
+      </div>
+      <div>
+        <button onClick={handlecountsub}>-</button>
+        <span>Count:{count}</span>
+        <button onClick={handlecountadd}>+</button>
+      </div>
+      <p>
+        <span>
+          {count === 0
+            ? "Today is"
+            : count > 0
+            ? `${Math.abs(count)} days from today is `
+            : `${Math.abs(count)}days ago was`}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
+    </div>
+  );
+}
